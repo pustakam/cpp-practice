cf23b8f36f0111c12246e68e0c8623a077a56a0a
pustakam <pustakam@users.noreply.github.com>
2019-01-30 20:55:59 -0600

    compile time computation etc

--- src/gottschling/meta.hpp
+++ src/gottschling/meta.hpp
@@ -1,6 +1,8 @@
 #ifndef __META_PROGRAMMING_H__
 #define __META_PROGRAMMING_H__
 
+#include <type_traits>
+
 constexpr unsigned long fibonacci(unsigned long n)
 {
     if (n > 1)
@@ -8,6 +10,31 @@ constexpr unsigned long fibonacci(unsigned long n)
 
     // 0 or 1
     return n;
+}
+
+template <typename T>
+constexpr T power(const T& x, unsigned int n)
+{
+    if (n == 0)
+        return 1;
+
+    // Using a while loop causes gcc
+    // to turn this function into runtime
+    return x * power(x, n - 1);
+}
+
+template <typename T, typename = std::enable_if<std::is_integral<T>::value>>
+constexpr bool is_prime_odd_div(T x, T div)
+{
+    return div >= x ? true :
+        (x % div == 0 ? false : is_prime_odd_div(x, div + 2));
+}
+
+template <typename T, typename = std::enable_if<std::is_integral<T>::value>>
+constexpr bool is_prime(T x)
+{    
+    return x == 1 ? false :
+        (x % 2 == 0 ? x == 2 : is_prime_odd_div(x, 3)); 
 }
 
 #endif // __META_PROGRAMMING_H__
--- src/gottschling/meta_test.cpp
+++ src/gottschling/meta_test.cpp
@@ -4,6 +4,11 @@
 
 TEST(MetaProgramming, fibonacci)
 {
+    static_assert(fibonacci(0) == 0, "fibonacci is not compile time");
+    static_assert(fibonacci(1) == 1, "fibonacci is not compile time");
+    static_assert(fibonacci(2) == 1, "fibonacci is not compile time");
+    static_assert(fibonacci(20) == 6765, "fibonacci is not compile time");
+
     ASSERT_TRUE(fibonacci(0) == 0);
     ASSERT_TRUE(fibonacci(1) == 1);
     ASSERT_TRUE(fibonacci(2) == 1);
@@ -18,3 +23,38 @@ TEST(MetaProgramming, fibonacci)
     ASSERT_TRUE(fibonacci(14) == 377);
     ASSERT_TRUE(fibonacci(20) == 6765);
 }
+
+TEST(MetaProgramming, power)
+{
+    static_assert(power(0.1, 0) == 1, "power is not compile time");
+    static_assert(power(1, 0) == 1, "power is not compile time");
+    static_assert(power(2, 0) == 1, "power is not compile time");
+    static_assert(power(2, 1) == 2, "power is not compile time");
+    static_assert(power(2, 2) == 4, "power is not compile time");
+    static_assert(power(2, 5) == 32, "power is not compile time");
+
+    ASSERT_TRUE(power(2, 10) == 1024);
+    ASSERT_NEAR(power(0.1, 1), 0.1, 0.01);
+    ASSERT_NEAR(power(0.1, 2), 0.01, 0.001);
+    ASSERT_NEAR(power(0.1, 3), 0.001, 0.0001);
+}
+
+TEST(MetaProgramming, is_prime)
+{
+    static_assert(!is_prime(1), "is_prime is not compile time");
+    static_assert(is_prime(2), "is_prime is not compile time");
+    static_assert(is_prime(3), "is_prime is not compile time");
+    static_assert(!is_prime(4), "is_prime is not compile time");
+    static_assert(is_prime(5), "is_prime is not compile time");
+    static_assert(!is_prime(6), "is_prime is not compile time");
+    static_assert(is_prime(7), "is_prime is not compile time");
+    static_assert(!is_prime(8), "is_prime is not compile time");
+    static_assert(!is_prime(9), "is_prime is not compile time");
+    static_assert(!is_prime(10), "is_prime is not compile time");
+
+    static_assert(is_prime(103), "is_prime is not compile time");
+    static_assert(!is_prime(1003), "is_prime is not compile time");
+
+    ASSERT_TRUE(is_prime(10007));
+}
+
